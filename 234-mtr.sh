89017eba1f8a8e57e411b00064ad5e99daf06227
Sébastien Gross <sebchezwamorg>
2025-02-12 18:10:04 +0100

    Add mtr
    This patch add mtr traceroute.
    
    Note that mtr-packet may require to be root setuid' or be cap'd to
    access to raw sockets:
    
        sudo setcap cap_net_raw+ep mtr-packet
    
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ 234-mtr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# May require extra capabilities see https://github.com/traviscross/mtr/blob/master/SECURITY
+
+set -e
+. "$(dirname $0)/env"
+cd $SRC
+
+if ! test -d mtr; then
+    git clone https://github.com/traviscross/mtr 
+fi
+cd mtr 
+
+./bootstrap.sh
+./configure \
+    --without-gtk \
+    --sbindir="$PREFIX/bin" \
+    --prefix="$PREFIX"
+
+make -j$(nproc)
+make install
