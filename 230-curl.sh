85d6f1443d40a3ca64d4dd17ac61a03f0ca2f642
Sébastien Gross <sebchezwamorg>
2025-08-28 21:10:15 +0200

    curl: simplify the build script
    This patch add some simplifications for the curl build script. Now
    curl is building fine.
    
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- 230-curl.sh
+++ 230-curl.sh
@@ -12,104 +12,72 @@ cd curl
 
 autoreconf -fi
 
-
-
+# need:
+#  apt-get install libldap-dev
+#  apt-get install libssh2-1-dev
+#  apt-get install libbrotli-dev
+#  apt-get install libgsasl-dev
+#  apt-get install libgss-dev
+#  apt-get install librtmp-dev
+#  apt-get install libkrb5-dev
 
 # aws-lc
-
 flavor=aws-lc
-# Do no compile aws-lc with libssh to prevent clash with libcrypto
-
-#  333  apt-get install libldap-dev
-#  335  apt-get install libssh2-1-dev
-#  337  apt-get install libgsasl-dev
-#  341  apt-get install libgss-dev
-#  345  apt-get install libidn2-dev
-#  349  apt-get install librtmp-dev
-#  353  apt-get install curl
-#  361  apt-get install libkrb5-dev
-
-#    --with-gsasl \
-#    --with-libidn2 \
-#    --enable-httpsrr \
-#    --enable-ech \
-#    --enable-ssls-export \
-#    --with-gssapi \
-
+PKG_CONFIG_PATH="$PREFIX/$flavor/lib/pkgconfig:$PREFIX/lib/pkgconfig" \
 ./configure \
-    PKG_CONFIG_PATH="$PREFIX/$flavor/lib/pkgconfig:$PREFIX/lib/pkgconfig" \
-    --without-gsasl \
-    --without-libidn2 \
-    --disable-httpsrr \
-    --disable-ech \
-    --disable-ssls-export \
-    --without-gssapi \
+    --with-libssh2 \
+    --with-gssapi \
+    --enable-httpsrr \
+    --enable-ech \
+    --enable-ssls-export \
     --with-openssl="$PREFIX/$flavor" \
     --with-nghttp3="$PREFIX" \
     --with-ngtcp2="$PREFIX/$flavor" \
     --prefix="$PREFIX/$flavor"
 make -j$(nproc)
-make install-strip
+make install #-strip
+
+
 cleanup_lib_version "$PREFIX/$flavor/lib/libcurl.so"
 add_lib_variant $PREFIX/$flavor/lib/libcurl.so $PREFIX/lib $flavor
-
-
-remove_version_needed $PREFIX/$flavor/bin/curl libnghttp2.so.14
 remove_version_needed $PREFIX/$flavor/bin/curl libcurl.so.4
-
 cp $PREFIX/$flavor/bin/curl $PREFIX/bin/curl-$flavor
+
 patchelf --replace-needed libcurl.so libcurl-$flavor.so  $PREFIX/bin/curl-$flavor
 
-for l in libssl libcrypto libnghttp2 libngtcp2_crypto_ossl libngtcp2 ; do
-    patchelf --replace-needed $l.so $l-$flavor.so $PREFIX/lib/libcurl-$flavor.so
-    patchelf --replace-needed $l.so $l-$flavor.so $PREFIX/bin/curl-$flavor
+#for l in libssl libcrypto libnghttp2 libngtcp2; do
+for l in libssl libcrypto libnghttp2 libngtcp2_crypto_ossl libngtcp2; do
+    patchelf --replace-needed $l.so $l-$flavor.so  $PREFIX/lib/libcurl-$flavor.so
 done
-
-LD_LIBRARY_PATH=$PREFIX/$flavor/lib $PREFIX/$flavor/bin/curl -V
-LD_LIBRARY_PATH=$PREFIX/lib $PREFIX/bin/curl-$flavor -V
 
 
 make clean
 flavor=openssl
 
 
-    # --with-libssh2 \
+PKG_CONFIG_PATH="$PREFIX/$flavor/lib/pkgconfig:$PREFIX/lib/pkgconfig" \
 ./configure \
-    PKG_CONFIG_PATH="$PREFIX/$flavor/lib/pkgconfig:$PREFIX/lib/pkgconfig" \
+    --with-libssh2 \
+    --with-gssapi \
+    --enable-httpsrr \
+    --enable-ssls-export \
     --with-openssl="$PREFIX/$flavor" \
-    --without-gsasl \
-    --without-libidn2 \
-    --disable-httpsrr \
-    --disable-ech \
-    --disable-ssls-export \
-    --without-gssapi \
     --with-nghttp3="$PREFIX" \
     --with-ngtcp2="$PREFIX/$flavor" \
     --prefix="$PREFIX/$flavor"
 
-
 make -j$(nproc)
-make install-strip
+make install #-strip
 
 
-# TODO: factorze
 cleanup_lib_version "$PREFIX/$flavor/lib/libcurl.so"
 add_lib_variant $PREFIX/$flavor/lib/libcurl.so $PREFIX/lib $flavor
-
-
-remove_version_needed $PREFIX/$flavor/bin/curl libnghttp2.so.14
 remove_version_needed $PREFIX/$flavor/bin/curl libcurl.so.4
-
 cp $PREFIX/$flavor/bin/curl $PREFIX/bin/curl-$flavor
+
 patchelf --replace-needed libcurl.so libcurl-$flavor.so  $PREFIX/bin/curl-$flavor
 
-for l in libssl libcrypto libnghttp2 libngtcp2_crypto_ossl libngtcp2 ; do
-    patchelf --replace-needed $l.so $l-$flavor.so $PREFIX/lib/libcurl-$flavor.so
-    patchelf --replace-needed $l.so $l-$flavor.so $PREFIX/bin/curl-$flavor
+#for l in libssl libcrypto libnghttp2 libngtcp2; do
+for l in libssl libcrypto libnghttp2 libngtcp2_crypto_ossl libngtcp2; do
+    patchelf --replace-needed $l.so $l-$flavor.so  $PREFIX/lib/libcurl-$flavor.so
 done
-
-LD_LIBRARY_PATH=$PREFIX/$flavor/lib $PREFIX/$flavor/bin/curl -V
-LD_LIBRARY_PATH=$PREFIX/lib $PREFIX/bin/curl-$flavor -V
-
-
-
